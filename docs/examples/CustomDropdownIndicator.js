618fdc96dd19487b1cdedddd64c06ba58704ee16
tskk97 <skktangirala@gmail.com>
2022-12-12 15:52:53 +0530

    Forked react-select v2.4.4 to include animations on dropdown

---
+++ docs/examples/CustomDropdownIndicator.js
@@ -0,0 +1,28 @@
+// @flow
+
+import React, { type ElementConfig } from 'react';
+import EmojiIcon from '@atlaskit/icon/glyph/emoji';
+import Select, { components } from '../../src';
+import { colourOptions } from '../data';
+
+const DropdownIndicator = (
+  props: ElementConfig<typeof components.DropdownIndicator>
+) => {
+  return (
+    <components.DropdownIndicator {...props}>
+      <EmojiIcon
+        primaryColor={colourOptions[2].color}
+      />
+    </components.DropdownIndicator>
+  );
+};
+
+export default () => (
+  <Select
+    closeMenuOnSelect={false}
+    components={{ DropdownIndicator }}
+    defaultValue={[colourOptions[4], colourOptions[5]]}
+    isMulti
+    options={colourOptions}
+  />
+);
